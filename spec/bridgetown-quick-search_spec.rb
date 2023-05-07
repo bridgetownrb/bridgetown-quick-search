b9004c4881edb4b88e732f94933405947e9feb8a
copiousfreetime <jeremy@copiousfreetime.org>
2023-05-06 23:42:56 -0600

    Add initial passing tests

--- spec/bridgetown-quick-search_spec.rb
+++ spec/bridgetown-quick-search_spec.rb
@@ -2,34 +2,66 @@
 
 require "spec_helper"
 
+# I am not sure why this is necessary, but it is the only way that the
+# initializer is actuall still run on every example, if the config is left in
+# the `let` then the initializer is only run on the first example
+module TestValues
+  class << self
+    attr_accessor :config
+  end
+end
+
+
 describe(Bridgetown::QuickSearch) do
-  let(:overrides) { {} }
   let(:config) do
-    Bridgetown.configuration(Bridgetown::Utils.deep_merge_hashes({
+    TestValues::config ||= Bridgetown.configuration(Bridgetown::Utils.deep_merge_hashes({
       "full_rebuild" => true,
       "root_dir"     => root_dir,
       "source"       => source_dir,
       "destination"  => dest_dir,
-    }, overrides))
-  end
-  let(:metadata_overrides) { {} }
-  let(:metadata_defaults) do
-    {
-      "name" => "My Awesome Site",
-      "author" => {
-        "name" => "Ada Lovejoy",
-      }
-    }
-  end
-  let(:site) { Bridgetown::Site.new(config) }
-  let(:contents) { File.read(dest_dir("index.html")) }
-  before(:each) do
-    metadata = metadata_defaults.merge(metadata_overrides).to_yaml.sub("---\n", "")
-    File.write(source_dir("_data/site_metadata.yml"), metadata)
-    site.process
-    FileUtils.rm(source_dir("_data/site_metadata.yml"))
+    }, overrides)).tap do |conf|
+      conf.run_initializers! context: :static
+    end
   end
 
-  it "doesn't have tests yet" do
+  describe "plugin behavior" do
+    let(:overrides) { {} }
+    let(:metadata_overrides) { {} }
+    let(:metadata_defaults) do
+      {
+        "name" => "My Awesome Site",
+        "author" => {
+          "name" => "Ada Lovejoy",
+        }
+      }
+    end
+
+    let(:site) { Bridgetown::Site.new(config) }
+    let(:contents) { File.read(dest_dir("index.html")) }
+    let(:index_json) { Pathname.new(dest_dir("bridgetown_quick_search/index.json")) }
+
+    before(:each) do
+      metadata = metadata_defaults.merge(metadata_overrides).to_yaml.sub("---\n", "")
+      File.write(source_dir("_data/site_metadata.yml"), metadata)
+      site.process
+      FileUtils.rm(source_dir("_data/site_metadata.yml"))
+    end
+
+    context "based upon page contents" do
+      it "creates the index.html file" do
+        expect(contents).to match 'Testing this plugin'
+      end
+
+      it "creates the index.json file" do
+        expect(index_json).to exist
+      end
+
+      it "has the content from the index.html file in the index" do
+        expect(index_json).to exist
+        parsed_index = JSON.parse(index_json.read)
+        root_entry = parsed_index.find { |entry| entry["url"] == "/" }
+        expect(root_entry).not_to be_nil
+        expect(root_entry["content"]).to match(/Testing this plugin/)
+      end
   end
 end
---
+++ spec/fixtures/config/initializers.rb
@@ -0,0 +1,5 @@
+# frozen_string_literals: true
+
+Bridgetown.configure do
+  init :"bridgetown-quick-search"
+end
